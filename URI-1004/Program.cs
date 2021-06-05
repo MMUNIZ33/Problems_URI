2e2c2edcbc3487a04d19474088a478648e0bb55d
Matheus Muniz <matthews.muniz33@gmail.com>
2021-06-04 23:09:45 -0400

    concluido 1004

--- URI-1004/Program.cs
+++ URI-1004/Program.cs
@@ -6,7 +6,13 @@ namespace URI_1004
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int a, b, PROD;
+            
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+
+            PROD = a * b;
+            Console.WriteLine("PROD = " + PROD);
         }
     }
 }
