d1803bb86720358a0a2de9b454acc331f31c9df8
Matheus Muniz <matthews.muniz33@gmail.com>
2021-06-05 19:09:34 -0400

    add 1005

---
+++ URI-1006/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace URI_1006
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
