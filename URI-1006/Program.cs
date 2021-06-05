e67607f46545aa1fd59530e89590c6924385c245
Matheus Muniz <matthews.muniz33@gmail.com>
2021-06-05 19:23:12 -0400

    concluido 1006

--- URI-1006/Program.cs
+++ URI-1006/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace URI_1006
 {
@@ -6,7 +7,15 @@ namespace URI_1006
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double A, B, C, media;
+            CultureInfo CI = CultureInfo.InvariantCulture;
+
+            A = double.Parse(Console.ReadLine(), CI);
+            B = double.Parse(Console.ReadLine(), CI);
+            C = double.Parse(Console.ReadLine(), CI);
+
+            media = ((A*2) + (B*3) + (C*5)) / 10;  
+            Console.WriteLine("MEDIA = " + media.ToString("F1", CI));
         }
     }
 }
