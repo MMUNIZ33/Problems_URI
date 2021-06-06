2ad93e0eac2134c99152a86d0130a7b6295d6121
Matheus Muniz <matthews.muniz33@gmail.com>
2021-06-06 15:34:38 -0400

    add uri area

---
+++ URI-AREA/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace URI_AREA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
