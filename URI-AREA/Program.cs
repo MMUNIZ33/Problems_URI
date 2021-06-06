d0a5e68c3a6f62a24ecac40b1d42e60c28b496d2
Matheus Muniz <matthews.muniz33@gmail.com>
2021-06-06 16:00:09 -0400

    concluido area

--- URI-AREA/Program.cs
+++ URI-AREA/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace URI_AREA
 {
@@ -6,7 +7,19 @@ namespace URI_AREA
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double bas, alt, area, perimetro, diagonal;
+            CultureInfo CI = CultureInfo.InvariantCulture;
+             
+            bas = double.Parse(Console.ReadLine(), CI);
+            alt = double.Parse(Console.ReadLine(), CI);
+
+            area = bas * alt;
+            perimetro = 2 * (bas + alt);
+            diagonal = Math.Sqrt(Math.Pow(bas, 2) + Math.Pow(area, 2));
+            
+            Console.WriteLine("AREA = " + area.ToString("F4", CI));
+            Console.WriteLine("PERIMETRO = " + perimetro.ToString("F4", CI));
+            Console.WriteLine("DIAGONAL = " + diagonal.ToString("F4", CI));
         }
     }
 }
