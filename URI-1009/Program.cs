f234cf18699b518a9d2c2a9f41cef052aee80bdc
Matheus Muniz <matthews.muniz33@gmail.com>
2021-06-07 22:21:33 -0400

    concluido 1009

--- URI-1009/Program.cs
+++ URI-1009/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace URI_1009
 {
@@ -6,7 +7,18 @@ namespace URI_1009
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string name;
+            double salary, bonuss, vendass;
+            double percentual = (15.0 / 100);
+            CultureInfo CI = CultureInfo.InvariantCulture;
+
+            name = Console.ReadLine();
+            salary = double.Parse(Console.ReadLine(), CI);
+            vendass = double.Parse(Console.ReadLine(), CI);
+
+            bonuss = vendass * percentual + salary;
+
+            Console.WriteLine("TOTAL = R$ " + bonuss.ToString("F2", CI));
         }
     }
 }
