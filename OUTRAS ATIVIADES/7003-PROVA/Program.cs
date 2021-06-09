36178dc6db541ff8643c46c12a1b23a6dfc397f7
Matheus Muniz <matthews.muniz33@gmail.com>
2021-06-09 12:10:16 -0400

    add 7003

---
+++ OUTRAS ATIVIADES/7003-PROVA/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _7003_PROVA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
