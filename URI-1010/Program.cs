acf35c6b73d10112e9bbad36a9651bb750254a17
Matheus Muniz <matthews.muniz33@gmail.com>
2021-06-09 01:08:03 -0400

    add 1010

---
+++ URI-1010/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace URI_1010
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
