910a1daf5eb7b89210e0ef417a2ca5655c5e14b0
Matheus Muniz <matthews.muniz33@gmail.com>
2021-06-09 00:10:10 -0400

    add 1019

---
+++ URI-1019/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace URI_1019
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
