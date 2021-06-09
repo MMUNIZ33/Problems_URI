0223f4e02002a7ea59c20ec34e564f770e7dcf86
Matheus Muniz <matthews.muniz33@gmail.com>
2021-06-08 22:14:57 -0400

    concluido media split

--- OUTRAS ATIVIADES/7002-SPLIT/Program.cs
+++ OUTRAS ATIVIADES/7002-SPLIT/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace _7002_SPLIT
 {
@@ -6,7 +7,22 @@ namespace _7002_SPLIT
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string name1, name2;
+            int idade1, idade2;
+            string[] vet;
+            double media;
+            CultureInfo CI = CultureInfo.InvariantCulture;
+
+            vet = Console.ReadLine().Split(" ");
+            name1 = vet[0];
+            idade1 = int.Parse(vet[1]);
+            vet = Console.ReadLine().Split(" ");
+            name2 = vet[0];
+            idade2 = int.Parse(vet[1]);
+
+            media = (idade1 + idade2) / 2.0;
+
+            Console.WriteLine("A idade média de {0} e {1} é de {2} anos.", name1, name2, media.ToString("F1", CI));
         }
     }
 }
