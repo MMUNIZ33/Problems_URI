565c70917a4a50be38950de10413cc084be6d9ad
Matheus Muniz <matthews.muniz33@gmail.com>
2021-06-14 19:59:10 -0400

    concluido 1011

---
+++ URI-1011/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace URI_1011
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CultureInfo CI = CultureInfo.InvariantCulture;
+            double raio, volume, pi;
+            pi = 3.14159;
+      
+       
+            raio = double.Parse(Console.ReadLine());
+       
+            volume = (4.0/3.0) * pi * (Math.Pow(raio, 3.0));
+
+            Console.WriteLine("VOLUME = " + volume.ToString("F3", CI));
+        }
+    }
+}
