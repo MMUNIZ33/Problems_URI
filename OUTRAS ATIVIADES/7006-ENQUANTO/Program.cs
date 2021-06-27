7ade960af858faeedb274134f38e93129ef467c4
Matheus Muniz <matthews.muniz33@gmail.com>
2021-06-27 16:43:35 -0400

    add 7006

---
+++ OUTRAS ATIVIADES/7006-ENQUANTO/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _7006_ENQUANTO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
