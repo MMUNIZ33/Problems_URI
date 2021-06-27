804b273b218ca288148b37399b418816e7f3cbc7
Matheus Muniz <matthews.muniz33@gmail.com>
2021-06-27 17:52:49 -0400

    concluido 7006

--- OUTRAS ATIVIADES/7006-ENQUANTO/Program.cs
+++ OUTRAS ATIVIADES/7006-ENQUANTO/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace _7006_ENQUANTO
 {
@@ -6,7 +7,32 @@ namespace _7006_ENQUANTO
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            CultureInfo CI = CultureInfo.InvariantCulture;
+            int cont;
+            double idade, media;
+            media = 0.0;
+            cont = 0;
+            idade = 0.0;
+           
+            idade = double.Parse(Console.ReadLine(), CI);
+
+            while (idade >= 0)
+            {
+                media = media + idade;
+                cont++;
+                idade = double.Parse(Console.ReadLine(), CI);
+            }
+
+
+            if (cont == 0)
+            {
+                System.Console.WriteLine("Impossível calcular");
+            } 
+            else
+            {
+                double result = media / cont;
+                Console.WriteLine(result.ToString("F2", CI));
+            }
         }
     }
 }
