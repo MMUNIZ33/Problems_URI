30e73c667c87dd141a43cb973f6dec7d473f29b9
Matheus Muniz <matthews.muniz33@gmail.com>
2021-06-27 16:39:57 -0400

    concluido 1113

---
+++ URI-1113/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace URI_1113
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y;
+            string[] z = Console.ReadLine().Split(' ');
+            x = int.Parse(z[0]);
+            y = int.Parse(z[1]);
+
+            while (x != y)
+            {
+                if (y < x)
+                {
+                    System.Console.WriteLine("Decrescente");
+                }
+                else (x < y)
+                {
+                    System.Console.WriteLine("Crescente");
+                }
+
+                z = Console.ReadLine().Split(' ');
+                x = int.Parse(z[0]);
+                y = int.Parse(z[1]);
+            }
+        }
+    }
+}
