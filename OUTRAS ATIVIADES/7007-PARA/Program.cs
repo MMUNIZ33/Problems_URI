992d6c9dabbaddc6b911d1088e27fe86f3a5cc52
Matheus Muniz <matthews.muniz33@gmail.com>
2021-06-27 18:14:02 -0400

    concluido 7007

--- OUTRAS ATIVIADES/7007-PARA/Program.cs
+++ OUTRAS ATIVIADES/7007-PARA/Program.cs
@@ -6,7 +6,18 @@ namespace _7007_PARA
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var x = 0;
+            var soma = 0;
+            int N;
+            N = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < N; i++)
+            {
+                x = int.Parse(Console.ReadLine());
+                soma = soma + x;
+            }
+
+            Console.WriteLine(soma);
         }
     }
 }
