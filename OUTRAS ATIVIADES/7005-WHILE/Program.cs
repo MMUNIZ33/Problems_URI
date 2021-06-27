fdf21e854b624c40359532b7659257aed6ba5d53
Matheus Muniz <matthews.muniz33@gmail.com>
2021-06-27 13:32:52 -0400

    add 7005

---
+++ OUTRAS ATIVIADES/7005-WHILE/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _7005_WHILE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
