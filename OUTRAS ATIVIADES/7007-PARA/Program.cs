e5a37cfd59e2a2161c1515ae0ebf16f776a07541
Matheus Muniz <matthews.muniz33@gmail.com>
2021-06-27 17:54:54 -0400

    add 7007

---
+++ OUTRAS ATIVIADES/7007-PARA/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _7007_PARA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
