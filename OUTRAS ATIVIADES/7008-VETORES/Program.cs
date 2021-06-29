1921cd09237164a292006a9a9cc9ce615e7e0a8d
Matheus Muniz <matthews.muniz33@gmail.com>
2021-06-28 22:58:42 -0400

    add 7008

---
+++ OUTRAS ATIVIADES/7008-VETORES/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _7008_VETORES
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
