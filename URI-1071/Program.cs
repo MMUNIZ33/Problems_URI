f81606855762faf879741f313fcd1473298d1ca5
Matheus Muniz <matthews.muniz33@gmail.com>
2021-06-28 22:54:40 -0400

    concluido 1071

--- URI-1071/Program.cs
+++ URI-1071/Program.cs
@@ -6,7 +6,32 @@ namespace URI_1071
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int x, y, min, max, soma;
+            x = int.Parse(Console.ReadLine());
+            y = int.Parse(Console.ReadLine());
+
+            if (x > y)
+            {
+                max = x;
+                min = y;
+            }
+            else
+            {
+                max = y;
+                min = x;
+            }
+
+            soma = 0;
+
+            for (int i = min + 1; i < max; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    soma =  soma + i;
+                }
+            }
+
+            System.Console.WriteLine(soma);
         }
     }
 }
