3751ee85da53e576c6b3b74ff7393d1001949e2c
Matheus Muniz <matthews.muniz33@gmail.com>
2021-06-30 19:49:46 -0400

    concluido 7009

--- OUTRAS ATIVIADES/7009-NEGATIVOS/Program.cs
+++ OUTRAS ATIVIADES/7009-NEGATIVOS/Program.cs
@@ -6,7 +6,24 @@ namespace _7009_NEGATIVOS
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int N;
+            int[] vet;
+            N = int.Parse(Console.ReadLine()); // ler quantidade de vetores de N
+            vet = new int[N];
+
+            string[] va = Console.ReadLine().Split(' ');
+
+            for (int i = 0; i < N; i++) // declarar vetores de N
+            {
+                vet[i] = int.Parse(va[i]);
+            }
+            for (int i = 0; i < N; i++) // imprimir vetores de N negativos
+            {
+                if (vet[i] < 0)
+                {
+                    System.Console.WriteLine(vet[i]);
+                }
+            }
         }
     }
 }
