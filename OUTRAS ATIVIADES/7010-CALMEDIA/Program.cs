27858a0b0570ba97abea1aa856e3ef45cd18a544
Matheus Muniz <matthews.muniz33@gmail.com>
2021-06-30 19:54:49 -0400

    add 7010

---
+++ OUTRAS ATIVIADES/7010-CALMEDIA/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _7010_CALMEDIA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
