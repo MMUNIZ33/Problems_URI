025fd453dd96d45f231490e431794a4f41b1609e
Matheus Muniz <matthews.muniz33@gmail.com>
2021-06-30 19:33:50 -0400

    add 7009

---
+++ OUTRAS ATIVIADES/7009-NEGATIVOS/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _7009_NEGATIVOS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
