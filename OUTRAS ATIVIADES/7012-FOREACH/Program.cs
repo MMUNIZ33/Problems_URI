3df0ce8f047c5c702232dd396149388e7b769de9
Matheus Muniz <matthews.muniz33@gmail.com>
2021-06-30 23:12:22 -0400

    concluido 7012

---
+++ OUTRAS ATIVIADES/7012-FOREACH/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace teste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N;
+            string[] nomes;
+            N = int.Parse(Console.ReadLine());
+            nomes = new string[N];
+
+            for (int i = 0; i < N; i++)
+            {
+                nomes[i] = Console.ReadLine();
+            }
+
+            Console.WriteLine("Nomes lidos:");
+            foreach (var item in nomes)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
