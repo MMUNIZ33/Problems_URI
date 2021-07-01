35b5e8b0f8ec3b7345c6edd11b3fd9ed06420a79
Matheus Muniz <matthews.muniz33@gmail.com>
2021-06-30 20:32:43 -0400

    concluido 7010

--- OUTRAS ATIVIADES/7010-CALMEDIA/Program.cs
+++ OUTRAS ATIVIADES/7010-CALMEDIA/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace _7010_CALMEDIA
 {
@@ -6,7 +7,33 @@ namespace _7010_CALMEDIA
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            CultureInfo CI = CultureInfo.InvariantCulture;
+            int N;
+            double[] vet;
+            var soma = 0.0;
+            var media = 0.0;
+            N = int.Parse(Console.ReadLine()); // ler quantidade de vetores de N
+            vet = new double[N];
+
+            string[] va = (Console.ReadLine().Split(' '));
+
+            for (int i = 0; i < N; i++) // declarar vetores de N
+            {
+                vet[i] = double.Parse(va[i], CI);
+            }
+            for (int i = 0; i < N; i++) // imprimir vetores de N
+            {
+                Console.Write(vet[i].ToString("F1", CI) + " ");
+            }
+            for (int i = 0; i < N; i++) //somar valores de N
+            {
+                soma = soma + vet[i];
+            }
+            
+            Console.WriteLine("\nSOMA = " + soma.ToString("F2", CI));
+            media = soma / N;
+            Console.WriteLine("MEDIA = " + media);
+
         }
     }
 }
