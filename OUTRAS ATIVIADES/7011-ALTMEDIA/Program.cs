a5c443ff0b1f7f6778749ac654b56290395156b9
Matheus Muniz <matthews.muniz33@gmail.com>
2021-06-30 22:00:37 -0400

    add 7011

---
+++ OUTRAS ATIVIADES/7011-ALTMEDIA/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _7011_ALTMEDIA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
