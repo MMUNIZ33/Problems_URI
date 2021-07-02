365d3e27489c35e9ecbdd8fe586f7565d0160936
Matheus Muniz <matthews.muniz33@gmail.com>
2021-07-02 01:28:30 -0400

    concluido 7013

--- OUTRAS ATIVIADES/7013-MATRIZES/Program.cs
+++ OUTRAS ATIVIADES/7013-MATRIZES/Program.cs
@@ -6,7 +6,35 @@ namespace _7013_MATRIZES
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int C, L;
+            int[,] A;
+            string[] s;
+
+            string[] va = Console.ReadLine().Split(' ');
+            L = int.Parse(va[0]);
+            C = int.Parse(va[1]);
+
+            A = new int[L, C];
+
+            // Atribuir valores de C e L
+            for (int i = 0; i < L; i++)
+            {
+                s = Console.ReadLine().Split(' ');
+                for (int j = 0; j < C; j++)
+                {
+                    A[i, j] = int.Parse(s[j]);
+                }
+            }
+
+            // Imprimir
+            for (int i = 0; i < L; i++)
+            {
+                for (int j = 0; j < C; j++)
+                {
+                    Console.Write(A[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
