249bbe12bc5f6f551d55430e98e22419f46dd4b6
Matheus Muniz <matthews.muniz33@gmail.com>
2021-07-02 18:19:34 -0400

    concluido 7014

--- OUTRAS ATIVIADES/7014-MTZQUA/Program.cs
+++ OUTRAS ATIVIADES/7014-MTZQUA/Program.cs
@@ -6,7 +6,45 @@ namespace _7014_MTZQUA
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int N, cont;
+            cont = 0;
+            int[,] mtz;
+            string[] x;
+
+            // Leitura de N
+            N = int.Parse(Console.ReadLine());
+            mtz = new int[N, N];
+
+            // Atribuir valores de mtz
+            for (int i = 0; i < N; i++)
+            {
+                x = Console.ReadLine().Split(' ');
+                for (int j = 0; j < N; j++)
+                {
+                    mtz[i, j] = int.Parse(x[j]);
+                }
+            }
+
+            // Imprimir diagonal de mtz
+            Console.Write("DIAGONAL PRINCIPAL: ");
+            for (int i = 0; i < N; i++)
+            {
+                Console.Write(mtz[i, i] + " ");
+            }
+
+            // Imprimir números negativos de mtz
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    if (mtz[i, j] < 0)
+                    {
+                        cont++;
+                        // Console.Write(mtz[i, j] + " ");
+                    }
+                }
+            }
+            Console.WriteLine("\nQUANTIDADE DE NEGATIVOS: " + cont);
         }
     }
 }
