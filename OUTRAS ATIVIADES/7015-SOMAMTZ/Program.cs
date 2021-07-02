8264fb16df186451f5c6520f2934ef5012d3da11
Matheus Muniz <matthews.muniz33@gmail.com>
2021-07-02 18:23:03 -0400

    add 7015

---
+++ OUTRAS ATIVIADES/7015-SOMAMTZ/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _7015_SOMAMTZ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
