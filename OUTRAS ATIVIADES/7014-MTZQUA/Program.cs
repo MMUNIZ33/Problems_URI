8ccfd260e2774bf330bd174c13a4c3aa9444e66f
Matheus Muniz <matthews.muniz33@gmail.com>
2021-07-02 17:27:55 -0400

    add 7014

---
+++ OUTRAS ATIVIADES/7014-MTZQUA/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _7014_MTZQUA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
